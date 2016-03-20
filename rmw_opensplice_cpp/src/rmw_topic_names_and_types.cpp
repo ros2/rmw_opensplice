3ae67b4b57f3431e8a53a99f29e2eaf0b8f5ac8b
ahcorde <ahcorde@gmail.com>
2016-03-20 17:10:25 +0100

    rmw_get_remote_topic_names_and_types implementation

--- rmw_opensplice_cpp/src/rmw_topic_names_and_types.cpp
+++ rmw_opensplice_cpp/src/rmw_topic_names_and_types.cpp
@@ -15,6 +15,7 @@
 #include <map>
 #include <set>
 #include <string>
+#include <sys/time.h>
 
 #include "rmw/allocators.h"
 #include "rmw/error_handling.h"
@@ -51,6 +52,167 @@ destroy_topic_names_and_types(
 // The extern "C" here enforces that overloading is not used.
 extern "C"
 {
+
+rmw_ret_t
+rmw_get_remote_topic_names_and_types(
+  rmw_topic_names_and_types_t * topic_names_and_types)
+{
+
+  if (!topic_names_and_types) {
+    RMW_SET_ERROR_MSG("topics handle is null");
+    return RMW_RET_ERROR;
+  }
+  if (topic_names_and_types->topic_count) {
+    RMW_SET_ERROR_MSG("topic count is not zero");
+    return RMW_RET_ERROR;
+  }
+  if (topic_names_and_types->topic_names) {
+    RMW_SET_ERROR_MSG("topic names is not null");
+    return RMW_RET_ERROR;
+  }
+  if (topic_names_and_types->type_names) {
+    RMW_SET_ERROR_MSG("type names is not null");
+    return RMW_RET_ERROR;
+  }
+
+  printf("rmw_topic_names_and_types_t\n");
+
+  DDS::DomainParticipantFactory_var dp_factory = DDS::DomainParticipantFactory::get_instance();
+  if (!dp_factory) {
+    RMW_SET_ERROR_MSG("failed to get domain participant factory");
+    return RMW_RET_ERROR;
+  }
+
+  DDS::DomainParticipant * participant = nullptr;
+
+  // Use the current ROS_DOMAIN_ID.
+  char * ros_domain_id = nullptr;
+  const char * env_var = "ROS_DOMAIN_ID";
+#ifndef _WIN32
+  ros_domain_id = getenv(env_var);
+#else
+  size_t ros_domain_id_size;
+  _dupenv_s(&ros_domain_id, &ros_domain_id_size, env_var);
+#endif  
+  size_t domain_id = std::stoi(ros_domain_id);
+
+  // On Windows, setting the ROS_DOMAIN_ID does not fix the problem, so error early.
+#ifdef _WIN32
+  if (!ros_domain_id) {
+    RMW_SET_ERROR_MSG("environment variable ROS_DOMAIN_ID is not set");
+    fprintf(stderr, "[rmw_opensplice_cpp]: error: %s\n", rmw_get_error_string_safe());
+    return RMW_RET_ERROR;
+  }
+#endif
+
+  printf("domain_id = %d\n", domain_id);
+
+  participant = dp_factory->create_participant(
+    domain_id, PARTICIPANT_QOS_DEFAULT, NULL, DDS::STATUS_MASK_NONE);
+  if (!participant) {
+    RMW_SET_ERROR_MSG("failed to create domain participant");
+    return RMW_RET_ERROR;
+  }
+
+  DDS::Subscriber * builtin_subscriber = participant->get_builtin_subscriber();
+  if (!builtin_subscriber) {
+    RMW_SET_ERROR_MSG("builtin subscriber handle is null");
+    return RMW_RET_ERROR;
+  }
+  
+  DDS::TopicBuiltinTopicDataDataReader * builtin_topic_datareader = NULL;
+  DDS::DataReader * data_reader = NULL;
+
+  data_reader = builtin_subscriber->lookup_datareader("DCPSTopic");
+  builtin_topic_datareader = DDS::TopicBuiltinTopicDataDataReader::_narrow(data_reader);
+
+  //timer variables
+  struct timeval a, b;
+  long totalb, totala;
+  long diff;
+
+  //get initial time
+  gettimeofday(&a, NULL);
+  totala = a.tv_sec + a.tv_usec/1000000;
+  bool timeout = false;
+
+  std::string substr = "::msg::dds_::";
+  std::map<std::string, std::string> topics;
+
+  while(!timeout){
+    DDS::TopicBuiltinTopicDataSeq_var topicDataSeq = new DDS::TopicBuiltinTopicDataSeq();
+    DDS::SampleInfoSeq_var infoSeq = new DDS::SampleInfoSeq();
+    builtin_topic_datareader->take(topicDataSeq, infoSeq, DDS::LENGTH_UNLIMITED,
+                          DDS::ANY_SAMPLE_STATE, DDS::ANY_VIEW_STATE, DDS::ANY_INSTANCE_STATE); //ALIVE_INSTANCE_STATE);
+
+    for(unsigned int i = 0; i < infoSeq->length(); ++i) {
+      DDS::TopicBuiltinTopicData_var topicData = topicDataSeq[i];
+      DDS::SampleInfo_var info = infoSeq[i];
+        switch(info->instance_state) {
+          case DDS::ALIVE_INSTANCE_STATE:
+            // reformat type name          
+            std::string type_name = std::string(topicData->type_name.m_ptr);
+            std::string name = std::string(topicData->name.m_ptr);
+            //printf("type_name 1: %s\n", type_name.c_str());
+            //printf("name 1: %s\n", name.c_str());
+            size_t substr_pos = type_name.find(substr);
+            if (type_name[type_name.size() - 1] == '_' && substr_pos != std::string::npos) {
+              type_name = type_name.substr(0, substr_pos) + "/" +   type_name.substr(
+                substr_pos + substr.size(),  type_name.size() - substr_pos - substr.size() - 1);
+              if(name.compare(std::string("ros_meta")) != 0){
+                printf("name: %s\t\ttype_name 2: %s\n", name.c_str(), type_name.c_str());
+                topics.insert ( std::pair<std::string, std::string> (name.c_str(), type_name.c_str()) );
+              }
+            }
+        }
+    }
+    gettimeofday(&b, NULL);
+    totalb = b.tv_sec + b.tv_usec/1000000;
+    diff = (totalb - totala);
+    // wait for 1 millisecond
+    if(diff > 10){
+      timeout = true;
+    }
+    usleep(100000); 
+  }
+
+    // copy data into result handle
+  if (topics.size() > 0) {
+    topic_names_and_types->topic_names = static_cast<char **>(
+      rmw_allocate(sizeof(char *) * topics.size()));
+    if (!topic_names_and_types->topic_names) {
+      RMW_SET_ERROR_MSG("failed to allocate memory for topic names")
+      return RMW_RET_ERROR;
+    }
+    topic_names_and_types->type_names = static_cast<char **>(
+      rmw_allocate(sizeof(char *) * topics.size()));
+    if (!topic_names_and_types->type_names) {
+      rmw_free(topic_names_and_types->topic_names);
+      RMW_SET_ERROR_MSG("failed to allocate memory for type names")
+      return RMW_RET_ERROR;
+    }
+    for (auto it : topics) {
+      char * topic_name = strdup(it.first.c_str());
+      if (!topic_name) {
+        RMW_SET_ERROR_MSG("failed to allocate memory for topic name")
+        return RMW_RET_ERROR;
+      }
+      char * type_name = strdup(it.second.c_str());
+      if (!type_name) {
+        rmw_free(topic_name);
+        RMW_SET_ERROR_MSG("failed to allocate memory for type name")
+        return RMW_RET_ERROR;
+      }
+      size_t i = topic_names_and_types->topic_count;
+      topic_names_and_types->topic_names[i] = topic_name;
+      topic_names_and_types->type_names[i] = type_name;
+      ++topic_names_and_types->topic_count;
+    }
+  }
+
+  return RMW_RET_OK;
+} 
+
 rmw_ret_t
 rmw_get_topic_names_and_types(
   const rmw_node_t * node,
