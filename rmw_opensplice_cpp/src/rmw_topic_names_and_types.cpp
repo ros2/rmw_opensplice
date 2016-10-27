f6e8ed0feafd958f5f4f196ca7312f5457826c06
Mikael Arguedas <mikael.arguedas@gmail.com>
2016-10-27 16:35:59 -0700

    uncrustify

--- rmw_opensplice_cpp/src/rmw_topic_names_and_types.cpp
+++ rmw_opensplice_cpp/src/rmw_topic_names_and_types.cpp
@@ -65,7 +65,7 @@ rmw_get_topic_names_and_types(
     RMW_SET_ERROR_MSG("node handle is not from this rmw implementation");
     return RMW_RET_ERROR;
   }
-  if(rmw_check_zero_rmw_topic_names_and_types(topic_names_and_types) != RMW_RET_OK) {
+  if (rmw_check_zero_rmw_topic_names_and_types(topic_names_and_types) != RMW_RET_OK) {
     return RMW_RET_ERROR;
   }
 
