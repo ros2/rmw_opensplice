5b3885ce2ff6fe985860958839d00c05200b8a17
Dirk Thomas <dthomas@osrfoundation.org>
2016-12-06 14:55:03 -0800

    support LIBRARY_NAME

--- rosidl_typesupport_opensplice_cpp/cmake/rosidl_typesupport_opensplice_cpp_generate_interfaces.cmake
+++ rosidl_typesupport_opensplice_cpp/cmake/rosidl_typesupport_opensplice_cpp_generate_interfaces.cmake
@@ -246,6 +246,10 @@ add_library(${rosidl_generate_interfaces_TARGET}${_target_suffix} SHARED
   ${_generated_external_msg_files}
   ${_generated_srv_files}
   ${_generated_external_srv_files})
+if(rosidl_generate_interfaces_LIBRARY_NAME)
+  set_target_properties(${rosidl_generate_interfaces_TARGET}${_target_suffix}
+    PROPERTIES OUTPUT_NAME "${rosidl_generate_interfaces_LIBRARY_NAME}${_target_suffix}")
+endif()
 if(NOT WIN32)
   set_target_properties(${rosidl_generate_interfaces_TARGET}${_target_suffix}
     PROPERTIES COMPILE_FLAGS "-std=c++11 -Wall -Wextra")
