9367aefe259d6dd529ec8b1619fe4549f41392f5
dhood <dhood@users.noreply.github.com>
2016-12-12 17:31:01 -0800

    Rename QoS policies

--- rmw_opensplice_cpp/src/qos.cpp
+++ rmw_opensplice_cpp/src/qos.cpp
@@ -27,10 +27,10 @@ bool set_entity_qos_from_profile(const rmw_qos_profile_t & qos_profile,
   DDSEntityQos & entity_qos)
 {
   switch (qos_profile.history) {
-    case RMW_QOS_POLICY_KEEP_LAST_HISTORY:
+    case RMW_QOS_POLICY_HISTORY_KEEP_LAST:
       entity_qos.history.kind = DDS::KEEP_LAST_HISTORY_QOS;
       break;
-    case RMW_QOS_POLICY_KEEP_ALL_HISTORY:
+    case RMW_QOS_POLICY_HISTORY_KEEP_ALL:
       entity_qos.history.kind = DDS::KEEP_ALL_HISTORY_QOS;
       break;
     case RMW_QOS_POLICY_HISTORY_SYSTEM_DEFAULT:
@@ -41,10 +41,10 @@ bool set_entity_qos_from_profile(const rmw_qos_profile_t & qos_profile,
   }
 
   switch (qos_profile.reliability) {
-    case RMW_QOS_POLICY_BEST_EFFORT:
+    case RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT:
       entity_qos.reliability.kind = DDS::BEST_EFFORT_RELIABILITY_QOS;
       break;
-    case RMW_QOS_POLICY_RELIABLE:
+    case RMW_QOS_POLICY_RELIABILITY_RELIABLE:
       entity_qos.reliability.kind = DDS::RELIABLE_RELIABILITY_QOS;
       break;
     case RMW_QOS_POLICY_RELIABILITY_SYSTEM_DEFAULT:
@@ -55,10 +55,10 @@ bool set_entity_qos_from_profile(const rmw_qos_profile_t & qos_profile,
   }
 
   switch (qos_profile.durability) {
-    case RMW_QOS_POLICY_TRANSIENT_LOCAL_DURABILITY:
+    case RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL:
       entity_qos.durability.kind = DDS::TRANSIENT_LOCAL_DURABILITY_QOS;
       break;
-    case RMW_QOS_POLICY_VOLATILE_DURABILITY:
+    case RMW_QOS_POLICY_DURABILITY_VOLATILE:
       entity_qos.durability.kind = DDS::VOLATILE_DURABILITY_QOS;
       break;
     case RMW_QOS_POLICY_DURABILITY_SYSTEM_DEFAULT:
