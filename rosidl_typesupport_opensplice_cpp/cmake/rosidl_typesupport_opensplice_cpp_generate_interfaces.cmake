853aba13aee42242cc4e921ad5289ca9f203b54a
Morgan Quigley <morgan@osrfoundation.org>
2016-12-13 14:43:34 -0800

    c++14 (#159)

--- rosidl_typesupport_opensplice_cpp/cmake/rosidl_typesupport_opensplice_cpp_generate_interfaces.cmake
+++ rosidl_typesupport_opensplice_cpp/cmake/rosidl_typesupport_opensplice_cpp_generate_interfaces.cmake
@@ -252,7 +252,7 @@ if(rosidl_generate_interfaces_LIBRARY_NAME)
 endif()
 if(NOT WIN32)
   set_target_properties(${rosidl_generate_interfaces_TARGET}${_target_suffix}
-    PROPERTIES COMPILE_FLAGS "-std=c++11 -Wall -Wextra")
+    PROPERTIES COMPILE_FLAGS "-std=c++14 -Wall -Wextra")
 endif()
 if(WIN32)
   target_compile_definitions(${rosidl_generate_interfaces_TARGET}${_target_suffix}
