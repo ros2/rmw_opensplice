028e0198dd5b99d5ff0bdd923624dce24038cd6c
William Woodall <william+github@osrfoundation.org>
2017-06-07 16:35:45 -0700

    create_client: avoid_ros_namespace_conventions not implemented

--- rmw_opensplice_cpp/src/rmw_client.cpp
+++ rmw_opensplice_cpp/src/rmw_client.cpp
@@ -56,6 +56,11 @@ rmw_create_client(
     RMW_SET_ERROR_MSG("qos_profile is null");
     return nullptr;
   }
+  
+  if (qos_profile->avoid_ros_namespace_conventions) {
+    RMW_SET_ERROR_MSG("QoS 'avoid_ros_namespace_conventions' is not implemented");
+    return NULL;
+  }
 
   auto node_info = static_cast<OpenSpliceStaticNodeInfo *>(node->data);
   if (!node_info) {
