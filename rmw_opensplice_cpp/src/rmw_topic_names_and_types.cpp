a15117290127b9acb4c7a77b15dc59884bf4c606
William Woodall <william@osrfoundation.org>
2017-06-15 19:42:43 -0700

    update to keep compiling with new rmw api changes

--- rmw_opensplice_cpp/src/rmw_topic_names_and_types.cpp
+++ rmw_opensplice_cpp/src/rmw_topic_names_and_types.cpp
@@ -16,8 +16,11 @@
 #include <set>
 #include <string>
 
+#include "rcutils/allocator.h"
 #include "rmw/allocators.h"
 #include "rmw/error_handling.h"
+#include "rmw/names_and_types.h"
+#include "rmw/get_topic_names_and_types.h"
 #include "rmw/rmw.h"
 #include "rmw/sanity_checks.h"
 #include "rmw/types.h"
@@ -25,38 +28,30 @@
 #include "identifier.hpp"
 #include "types.hpp"
 
-RMW_LOCAL
-void
-destroy_topic_names_and_types(
-  rmw_topic_names_and_types_t * topic_names_and_types)
-{
-  if (topic_names_and_types->topic_count) {
-    for (size_t i = 0; i < topic_names_and_types->topic_count; ++i) {
-      delete topic_names_and_types->topic_names[i];
-      delete topic_names_and_types->type_names[i];
-      topic_names_and_types->topic_names[i] = nullptr;
-      topic_names_and_types->type_names[i] = nullptr;
-    }
-    if (topic_names_and_types->topic_names) {
-      rmw_free(topic_names_and_types->topic_names);
-      topic_names_and_types->topic_names = nullptr;
-    }
-    if (topic_names_and_types->type_names) {
-      rmw_free(topic_names_and_types->type_names);
-      topic_names_and_types->type_names = nullptr;
-    }
-    topic_names_and_types->topic_count = 0;
-  }
-}
-
 // The extern "C" here enforces that overloading is not used.
 extern "C"
 {
 rmw_ret_t
 rmw_get_topic_names_and_types(
   const rmw_node_t * node,
-  rmw_topic_names_and_types_t * topic_names_and_types)
+  rcutils_allocator_t * allocator,
+  bool no_demangle,
+  rmw_names_and_types_t * topic_names_and_types)
 {
+  (void)node;
+  (void)allocator;
+  (void)no_demangle;
+  (void)topic_names_and_types;
+  /* This function is disabled because it does not implement this function correctly anymore.
+   * To be reenabled it must (at least):
+   *
+   * - use the given allocator
+   * - support the no_demangle, or explicitly fail if it is true
+   * - support the new rmw_names_and_types data structure which has multiple types per topic
+   */
+  RMW_SET_ERROR_MSG("not implemented")
+  return RMW_RET_ERROR;
+/*
   if (!node) {
     RMW_SET_ERROR_MSG("node handle is null");
     return RMW_RET_ERROR;
@@ -146,17 +141,6 @@ rmw_get_topic_names_and_types(
 fail:
   destroy_topic_names_and_types(topic_names_and_types);
   return RMW_RET_ERROR;
-}
-
-rmw_ret_t
-rmw_destroy_topic_names_and_types(
-  rmw_topic_names_and_types_t * topic_names_and_types)
-{
-  if (!topic_names_and_types) {
-    RMW_SET_ERROR_MSG("topics handle is null");
-    return RMW_RET_ERROR;
-  }
-  destroy_topic_names_and_types(topic_names_and_types);
-  return RMW_RET_OK;
+*/
 }
 }  // extern "C"
