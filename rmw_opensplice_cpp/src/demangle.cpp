43fbf5f401c817acbad482b91436de4065415045
Chris Lalancette <clalancette@openrobotics.org>
2018-09-05 18:28:11 +0000

    Add semicolons to all RCLCPP and RCUTILS macros.
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- rmw_opensplice_cpp/src/demangle.cpp
+++ rmw_opensplice_cpp/src/demangle.cpp
@@ -85,7 +85,7 @@ _demangle_service_from_topic(const std::string & topic_name)
       if (topic_name.length() - suffix_position - suffix.length() != 0) {
         RCUTILS_LOG_WARN_NAMED("rmw_opensplice_cpp",
           "service topic has service prefix and a suffix, but not at the end"
-          ", report this: '%s'", topic_name.c_str())
+          ", report this: '%s'", topic_name.c_str());
         continue;
       }
       found_suffix = suffix;
@@ -95,7 +95,7 @@ _demangle_service_from_topic(const std::string & topic_name)
   if (suffix_position == std::string::npos) {
     RCUTILS_LOG_WARN_NAMED("rmw_opensplice_cpp",
       "service topic has prefix but no suffix"
-      ", report this: '%s'", topic_name.c_str())
+      ", report this: '%s'", topic_name.c_str());
     return "";
   }
   // strip off the suffix first
@@ -126,7 +126,7 @@ _demangle_service_type_only(const std::string & dds_type_name)
       if (dds_type_name.length() - suffix_position - suffix.length() != 0) {
         RCUTILS_LOG_WARN_NAMED("rmw_opensplice_cpp",
           "service type contains '::srv::dds_::' and a suffix, but not at the end"
-          ", report this: '%s'", dds_type_name.c_str())
+          ", report this: '%s'", dds_type_name.c_str());
         continue;
       }
       found_suffix = suffix;
@@ -136,7 +136,7 @@ _demangle_service_type_only(const std::string & dds_type_name)
   if (suffix_position == std::string::npos) {
     RCUTILS_LOG_WARN_NAMED("rmw_opensplice_cpp",
       "service type contains '::srv::dds_::' but does not have a suffix"
-      ", report this: '%s'", dds_type_name.c_str())
+      ", report this: '%s'", dds_type_name.c_str());
     return "";
   }
   // everything checks out, reformat it from '<pkg>::srv::dds_::<type><suffix>' to '<pkg>/<type>'
